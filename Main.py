2fdc06aa789f2cc8453d0096c89767efdcb3c237
Corey BROOKE <brocy076@mymail.unisa.edu.au>
2025-10-18 21:58:12 +1100

    docs: added details to Docstrings on each python file

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 """
 File: main.py
 Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Author: Corey Brooke
+ID: 110480857
+Username: Brocy076
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
