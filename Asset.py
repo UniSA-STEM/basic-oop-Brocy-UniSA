1da36232a03ffa163bf00443d7708e893d0073b4
Corey BROOKE <brocy076@mymail.unisa.edu.au>
2025-10-19 19:29:41 +1100

    feat: created Asset class with base parameters and basic plan
    - Added new Asset class in Asset.py
    - Set up the starting parameters for the class
    - Outlined the functions I will be using

--- Asset.py
+++ Asset.py
@@ -6,3 +6,22 @@ ID: 110480857
 Username: Brocy076
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+from Hacker import Hacker
+from Rig import Rig
+
+class Asset():
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.encrypted = False
+
+    def transfer(self):
+        """Changes the inventory it is stored in
+
+        something like if its in one move to other, but i will need a class to check what one it is in"""
+        pass
+
+
+    def __str__(self):
+        """Return a string representation of the Asset."""
+        pass
