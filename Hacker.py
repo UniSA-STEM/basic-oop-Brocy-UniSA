d8f7b1cda7ac2ad4ca9fc032ecbebbebdad3524a
Corey BROOKE <brocy076@mymail.unisa.edu.au>
2025-10-20 07:34:04 +1100

    feat: integrated Asset class into Hacker and updated acquire_rig to work with said Asset class
    - Added Asset class into Hacker class
    - Updated acquire_rig() to work with asset inventory system

--- Hacker.py
+++ Hacker.py
@@ -6,6 +6,7 @@ ID: 110480857
 Username: Brocy076
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+from Asset import *
 from Rig import Rig
 
 
@@ -22,24 +23,24 @@ class Hacker:
         self.__name = name
         self.__trace_level = 0
         self.__trace_limit = 5
-        self.__detected = False
-        self.__inventory = ["CryptoToken"]  # TODO: Make the inventory better. (this is just a placeholder)
         self.__rig = None
+        self.__inventory = [CryptoToken()]
 
-    def acquire_rig(self, rig=None):
-        """
-        This function is used to give a rig to the Hacker.
+    def acquire_rig(self, rig: Rig | None = None):
+        """Acquire a rig using one CryptoToken.
 
-        If the hacker has a CryptoToken, it is removed from the inventory and
-        a rig is set, while telling the user that it is activated.
-        If the user doesn't have enough tokens a message is printed indicating such.
-        """
-        if "CryptoToken" in self.__inventory:
-            self.__inventory.remove("CryptoToken")
-            self.__rig = rig if rig else Rig()
-            print(f"{self.__name}, your rig is activated and ready for use.")
-        else:
+        Searches the inventory for the first CryptoToken. If found, it is removed
+        and a new rig is acquired (or the provided one is activated)."""
+    # Finds the first CryptoToken in the inventory list or returns None
+        token = next((asset for asset in self.__inventory if asset.name == "CryptoToken"), None)
+        if not token:
             print(f"{self.__name} doesn’t have enough CryptoTokens to buy a rig.")
+            return
+
+        # Removes the token from the list and creates the Rig
+        self.__inventory.remove(token)
+        self.__rig = rig if rig else Rig()
+        print(f"{self.__name}, your rig is activated and ready for use.")
 
     def get_trace_level(self):
         return self.__trace_level
@@ -74,9 +75,18 @@ class Hacker:
         """I will need to work out the best way to do all the inventory functions"""
         return self.__inventory
 
-    def transfer_inventory(self):
-        """Prone to change likely done with assest class"""
-        pass
+    def remove_asset(self, asset):
+        if asset in self.__inventory:
+            self.__inventory.remove(asset)
+
+    def transfer_asset(self, asset, destination):
+        """Transfer an asset from this hacker to another rig or hacker."""
+        if asset in self.__inventory:
+            self.remove_asset(asset)
+            destination.add_asset(asset)
+            print(f"{asset} transferred from {self.__name} to {destination}.")
+        else:
+            print(f"{asset} not found in {self.__name}'s inventory.")
 
     def __str__(self):
         """Return a string representation of the Hacker."""
